57646aea0e946fff3b9590ca561eeb83ebb967de
emad-abuhamdeh <112353622+emad-abuhamdeh@users.noreply.github.com>
2023-09-20 20:49:24 -0700

    Initialize blank

---
+++ nuxt.config.ts
@@ -0,0 +1,80 @@
+// import { fileURLToPath, URL } from 'node:url'
+import presetIcons from '@unocss/preset-icons'
+
+export default defineNuxtConfig({
+  // ssr: false,
+  runtimeConfig: {
+    public: {
+      title: `Vue Designer`,
+      description: 'Vue Designer Nuxt Tailwind CSS - Quick start template',
+      author: 'Pinegrow',
+      nav: [
+        { text: 'Home', link: '/' },
+        { text: `Quick Start`, link: '/quick-start' },
+        { text: 'Subscribe', link: '/subscribe' },
+      ],
+    },
+    app: {
+      baseURL: '/',
+    },
+  },
+  modules: [
+    '@pinegrow/nuxt-module',
+    '@unocss/nuxt',
+    // '@nuxt/devtools',
+    '@nuxt/content',
+    '@vueuse/nuxt',
+    '@nuxtjs/html-validator',
+  ],
+  pinegrow: {
+    liveDesigner: {
+      iconPreferredCase: 'unocss', // default value (can be removed), unocss by default uses the unocss format for icon names names
+      devtoolsKey: 'devtools', // see plugins/devtools.client.ts
+      tailwindcss: {
+        configPath: 'tailwind.config.ts',
+        cssPath: '@/assets/css/tailwind.css',
+      },
+      // plugins: [
+      //   {
+      //     name: 'My Awesome Lib 3.0',
+      //     key: 'my-awesome-lib',
+      //     pluginPath: fileURLToPath(
+      //       new URL('./my-awesome-lib/web-types.json', import.meta.url),
+      //     ),
+      //   },
+      // ],
+    },
+  },
+  css: ['~/assets/css/tailwind.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  content: {
+    markdown: {
+      anchorLinks: false,
+      rehypePlugins: [
+        [
+          'rehype-external-links',
+          {
+            target: '_blank',
+            rel: ['noopener'],
+            test: (node: any) => /^https?:\/\//.test(node.properties.href),
+          },
+        ],
+      ],
+    },
+    highlight: {
+      theme: 'dracula-soft',
+    },
+  },
+  unocss: {
+    presets: [
+      presetIcons({
+        prefix: 'i-', // default prefix, do not change
+      }),
+    ],
+  },
+})
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+  definePageMeta({
+    title: 'Home',
+    navOrder: 1,
+  })
+
+  useHead({
+    title: () => 'Home',
+  })
+</script>
+<template>
+  <main><span>A Fresh Start!</span></main>
+</template>
+<style scoped></style>
